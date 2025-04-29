5f4a3c6cf3e3a8905ad41dbb4617ed13f9385e95
Wendley Santos <wendlley007@users.noreply.github.com>
2025-04-29 11:03:30 -0300

    Corrigindo problemas de capitalização nos componentes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,9 @@ import About from "../components/About";
 import Projects from "../components/Projects";
 import Certificates from "../components/Certificates";
 import Contact from "../components/Contact";
+import Hero from "../components/Hero";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Hero from "../components/hero";
 
 export default function Home() {
   return (
