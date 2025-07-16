4f401d06507d586f8ced94a0a95e56aca4e6972d
Wendley Santos <wendlley007@users.noreply.github.com>
2025-07-16 12:05:50 -0300

    Correções

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
 import React from "react";
 import Home from "./pages/Home";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <div className="font-sans min-h-screen bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white">
-      <Home />
-    </div>
+    <>
+      <Helmet>
+        <title>Wendley.dev | Portfólio</title>
+        <meta
+          name="description"
+          content="Portfólio de Wendley, desenvolvedor web focado em criar interfaces modernas, responsivas e experiências memoráveis."
+        />
+        <meta name="author" content="Wendley Santos Ribeiro" />
+        <link rel="icon" href="/assets/code.png" type="image/png" />
+
+        {/* SEO estruturado (JSON-LD) */}
+        <script type="application/ld+json">
+          {`
+            {
+              "@context": "https://schema.org",
+              "@type": "Person",
+              "name": "Wendley Santos Ribeiro",
+              "url": "https://wendleydev.vercel.app",
+              "image": "https://wendleydev.vercel.app/assets/perfil2.webp",
+              "sameAs": [
+                "https://www.linkedin.com/in/wendley-santos-248159219",
+                "https://github.com/Wendley007",
+                "https://wa.me/5538999272911"
+              ],
+              "jobTitle": "Desenvolvedor Web Front-end",
+              "worksFor": {
+                "@type": "Organization",
+                "name": "Freelancer"
+              },
+              "description": "Portfólio de Wendley, desenvolvedor web com foco em criar interfaces modernas, responsivas e experiências únicas com React, Tailwind e JavaScript.",
+              "email": "mailto:wsanttossttreett@gmail.com",
+              "address": {
+                "@type": "PostalAddress",
+                "addressCountry": "BR"
+              }
+            }
+          `}
+        </script>
+      </Helmet>
+
+      <div className="font-sans min-h-screen bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white">
+        <Home />
+      </div>
+    </>
   );
 }
 
